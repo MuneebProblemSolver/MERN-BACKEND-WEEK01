f4693c566106705629da9899085db5f67e5c85a3
Malik's MacBook Pro <bismillah@Maliks-MacBook-Pro.local>
2024-06-14 15:04:32 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+require("./db/conn"); // Assuming your database connection logic is in db/conn.js
+const cors = require("cors");
+const router = require("./Routes/router");
+const PORT = process.env.PORT || 6010;
+const mongoose = require('mongoose');
+
+// Configure CORS to allow requests from port 3000 (your React frontend)
+const corsOptions = {
+  origin: "http://localhost:3000", // Replace with your frontend URL if deployed elsewhere
+  credentials: true, // Allow cookies for authentication purposes (if applicable)
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Origin", "Accept"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
+};
+
+app.use(cors(corsOptions)); // Apply CORS middleware with options
+
+app.use(express.json());
+app.use("/uploads", express.static("./uploads"));
+app.use("/files", express.static("./public/files"));
+mongoose.set('strictQuery', false);
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server started at port no ${PORT}`);
+});
---
+++ db/conn.js
@@ -0,0 +1,12 @@
+
+
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb+srv://AdminUser:malik0786@mongocluster.u92cud5.mongodb.net/Cruds?retryWrites=true&w=majority&appName=MongoCluster';
+
+mongoose.set('strictQuery', false);  // Add this line before or after connect
+
+
+mongoose.connect(connectionString)
+    .then(() => console.log('MongoDB database connected successfully!'))
+    .catch(error => console.error('MongoDB connection error:', error));
