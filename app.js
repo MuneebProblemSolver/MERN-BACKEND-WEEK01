f3f8a232ec794d6326bf8b4b8d3b7aedd5bfd523
Malik's MacBook Pro <bismillah@Maliks-MacBook-Pro.local>
2024-06-15 17:07:47 +0500

    New

--- app.js
+++ app.js
@@ -9,12 +9,7 @@ const mongoose = require('mongoose');
 
 // Configure CORS to allow requests from the frontend origin specified in .env
 const corsOptions = {
-<<<<<<< HEAD
-  origin: process.env.FRONTEND_ORIGIN, // Use environment variable for frontend origin
-=======
-  origin: "h process.env.FRONTEND_ORIGIN", // Replace with your frontend URL if deployed elsewhere
->>>>>>> a6539194522df973d89a8aa781b0cf5ec12b88fb
-  credentials: true, // Allow cookies for authentication purposes (if applicable)
+  origin: process.env.FRONTEND_ORIGIN, // Use environment variabl
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Origin", "Accept"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
 };
